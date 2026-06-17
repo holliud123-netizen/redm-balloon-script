11a70e5772c99fb977bdf37f58cf67d7dc099377
holliud123-netizen <holliud123@gmail.com>
2026-06-16 22:01:47 -0300

    Add RedM pre-release warning to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,7 @@
 fx_version 'cerulean'
 game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
 author 'RedM Developer'
 description 'Professional Hot Air Balloon Script'
 version '1.0.0'
